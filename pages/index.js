f725dbf4e0e23b311fc79a958ff92a6e55a68b31
divyanshukaushik <divyanshukaushik33@gmail.com>
2021-11-14 14:56:37 +0530

    project upload

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>News App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <div className="bg-gray-900 opacity-90 p-24 rounded-2xl">
+        <h3 className="text-gray-200 text-7xl">heljl</h3>
+      </div>
+      </main>
+
+    </div>
+  )
+}
